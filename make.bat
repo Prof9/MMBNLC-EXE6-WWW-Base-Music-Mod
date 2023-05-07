31d1cfbdb632b2b4affcf75a39273d17ad42c58e
Prof. 9 <prof9@outlook.com>
2023-05-08 01:10:10 +0200

    Add make script and remaining info files

--- make.bat
+++ make.bat
@@ -1,3 +1,106 @@
 @echo off
-cargo build --release
-copy "target\release\patch.dll" "C:\Program Files (x86)\Steam\steamapps\common\MegaMan_BattleNetwork_LegacyCollection_Vol2\exe\mods\FixWWWBaseMusic_EXE6"
+setlocal enabledelayedexpansion
+
+rem Mod info
+set "MOD_DIR=PostgameWWWBaseMusic_EXE6"
+
+rem Install locations
+set "VOL2_DIR=C:\Program Files (x86)\Steam\steamapps\common\MegaMan_BattleNetwork_LegacyCollection_Vol2"
+
+rem Build folder
+set "BUILD_DIR=_build"
+set "BUILD_DIR_VOL2=!BUILD_DIR!\!MOD_DIR!"
+set "INSTALL_DIR_VOL2=!VOL2_DIR!\exe\mods\!MOD_DIR!"
+
+if /I [%1]==[clean] (
+	echo Running cargo clean...
+	cargo clean ^
+		1> nul || goto :error
+	echo Removing build folder...
+	if exist "!BUILD_DIR!" (
+		rmdir /S /Q "!BUILD_DIR!" ^
+			1> nul || goto :error
+	)
+	echo.
+
+	goto :done
+)
+
+if /I [%1]==[install] (
+	if exist "!VOL2_DIR!" (
+		if exist "!BUILD_DIR_VOL2!" (
+			echo Installing for Volume 2...
+
+			echo Copying mod folder...
+			if exist "!INSTALL_DIR_VOL2!" (
+				del /F /S /Q "!INSTALL_DIR_VOL2!\*" 1> nul || goto :error
+			) else (
+				mkdir "!INSTALL_DIR_VOL2!" 1> nul || goto :error
+			)
+			robocopy /E "!BUILD_DIR_VOL2!" "!INSTALL_DIR_VOL2!" 1> nul
+			if errorlevel 8 goto :error
+		) else (
+			echo Volume 2 not built; skipping...
+		)
+	) else (
+		echo Volume 2 not installed; skipping...
+	)
+	echo.
+
+	goto :done
+)
+
+if /I [%1]==[uninstall] (
+	if exist "!VOL2_DIR!" (
+		echo Uninstalling for Volume 2...
+		if exist "!INSTALL_DIR_VOL2!" (
+			rmdir /S /Q "!INSTALL_DIR_VOL2!" ^
+				1> nul || goto :error
+		)
+	) else (
+		echo Volume 2 not installed; skipping...
+	)
+	echo.
+
+	goto :done
+)
+
+rem Build mod
+echo Building for Volume 2...
+
+rem Clean build folder
+call :clean_folder "!BUILD_DIR_VOL2!"
+
+echo Running cargo build...
+cargo build --release ^
+	1> nul || goto :error
+
+echo Copying mod files...
+copy "target\release\patch.dll" "!BUILD_DIR_VOL2!" ^
+	1> nul || goto :error
+copy /Y "info.toml" "!BUILD_DIR_VOL2!\info.toml" ^
+	1> nul || goto :error
+copy /Y "init.lua" "!BUILD_DIR_VOL2!\init.lua" ^
+	1> nul || goto :error
+echo.
+
+rem Copy miscellaneous files
+copy /Y "readme.md" "!BUILD_DIR!\readme.txt" ^
+	1> nul || goto :error
+
+:done
+echo Done.
+echo.
+exit /b 0
+
+:error
+echo Error occurred, failed to build.
+echo.
+exit /b 1
+
+:clean_folder
+if exist "%1" (
+	del /F /S /Q "%1\*" 1> nul || goto :error
+) else (
+	mkdir "%1" 1> nul || goto :error
+)
---
+++ readme.md
@@ -0,0 +1,55 @@
+MMBNLC Postgame WWW Base Music mod
+==================================
+
+This is a mod for Mega Man Battle Network Legacy Collection Vol. 2 adjusts the
+field music played in the WWW base in Mega Man Battle Network 6 after the game
+is completed.
+
+Normally, after beating the game, the original Expo Site music will play in that
+area again from before it was revealed to be the WWW base. This mod makes it so
+that the WWW base music will continue to play in that area. All other areas will
+have their normal area music.
+
+
+Features
+--------
+
+* After beating the game, WWW base music is kept as-is rather than reverting to
+  the original Expo Site theme.
+
+
+Installing
+----------
+
+Windows PC and Steam Deck
+
+1. Download and install chaudloader: https://github.com/RockmanEXEZone/chaudloader/releases Version 0.8.1 or newer is required.
+
+2. Launch Steam in Desktop Mode. Right-click the game in Steam, then click Properties → Local Files → Browse to open the game's install folder. Then open the "exe" folder, where you'll find MMBN_LC2.exe.
+
+3. Copy the PostgameWWWBaseMusic_EXE6 folder to the "mods" folder.
+
+4. Launch the game as normal.
+
+
+Version History
+---------------
+
+Ver. 1.0.0 - 8 May 2023
+
+* Initial version.
+
+
+Building
+--------
+
+Building is supported on Windows 10 & 11. First install the following prerequisites:
+
+* Rust
+
+Then, run one of the following commands:
+
+* make - Builds the mod files compatible with chaudloader.
+* make clean - Removes all temporary files and build outputs.
+* make install - Installs the previously built mod files into the mods folder for chaudloader.
+* make uninstall - Removes the installed mod files from the mods folder for chaudloader.
