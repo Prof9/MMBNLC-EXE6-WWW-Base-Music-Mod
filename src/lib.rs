66c3c00f87350b6468343459f14920076737fe25
Prof. 9 <prof9@outlook.com>
2023-05-08 00:35:55 +0200

    Update GBA struct, search within .xtext section only

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,24 @@ use ilhook::x64::{Hooker, HookPoint, HookType, JmpToRetRoutine, Registers, Callb
 use std::os::raw::{c_int, c_void};
 use winapi::{shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE}, um::winnt::DLL_PROCESS_DETACH};
 
-mod memsearch;
+pub mod memsearch;
 
 #[derive(Copy, Clone, Debug)]
-#[repr(C)]
+#[repr(C, packed)]
 pub struct GBAState {
     pub r0:  u32,  pub r1:  u32,  pub r2:  u32,  pub r3:  u32,
     pub r4:  u32,  pub r5:  u32,  pub r6:  u32,  pub r7:  u32,
     pub r8:  u32,  pub r9:  u32,  pub r10: u32,  pub r11: u32,
     pub r12: u32,  pub r13: u32,  pub r14: u32,  pub r15: u32,
-    pub flags1: u32,  pub flags2: u32,
+    pub flags: u32,  pub flags_enabled: u32,
     pub ram: *const u8,
-    // TODO more stuff
+    pub unk50: u32, pub unk54: u32, pub unk58: u32, pub unk5c: u32,
+    pub ldmia_stmia_addr: u32,
+    pub stack_size: u32, pub call_depth: u32,
 }
 
 impl GBAState {
-    pub fn ram_u8(&self, addr: u32) -> u8 {
+    pub fn read_u8(&self, addr: u32) -> u8 {
         unsafe { *(self.ram.offset(addr.try_into().unwrap())) }
     }
 
@@ -52,10 +54,16 @@ fn hook_direct(addr: usize, func: JmpToRetRoutine) {
 }
 
 fn hook_search(what: &str, n: usize, func: JmpToRetRoutine) {
+    let image_base: usize = 0x140000000 as usize;
+    let xtext_start: usize = unsafe { *((image_base+0x1EC) as *const u32) as usize + image_base };
+    let xtext_size: usize = unsafe { *((image_base+0x1F0) as *const u32) as usize };
+
     println!("Searching for: {what}");
     let query = memsearch::Query::build(what).expect("query string should be valid");
-    println!("Built search query");
-    let matches = query.iter_matches_in(0x140000000, 0x10000000).take(n);
+    println!("Query built");
+    let matches = query
+        .iter_matches_in(xtext_start, xtext_size)
+        .take(n);
     for addr in matches {
         hook_direct(addr, func);
     }
@@ -68,9 +76,10 @@ pub unsafe extern "C" fn luaopen_patch(_: c_void) -> c_int {
 }
 
 extern "win64" fn on_hook(reg: *mut Registers, ori_func_ptr: usize, _user_data: usize) -> usize {
+    // When we get here, the game has already performed a check for incident music fix
     let gba = unsafe { GBAState::from_addr((*reg).rbx) };
 
-    let area = gba.ram_u8(gba.r6 + 4);
+    let area = gba.read_u8(gba.r6 + 4);
 
     gba.r0 = match area {
         0x06 => 0x62, // If in WWW Base, use music state with correct WWW Base music
